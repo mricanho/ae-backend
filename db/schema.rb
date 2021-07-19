f89bc63e2bb1ad1c2ecd3a554e79654fd94e4f92
Miguel Ricaño <mricanho@gmail.com>
2021-07-19 16:53:10 -0500

    Add foreign key to appointment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_214633) do
+ActiveRecord::Schema.define(version: 2021_07_19_215157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_07_19_214633) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "lecture_id", null: false
+    t.index ["lecture_id"], name: "index_appointments_on_lecture_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
 
@@ -39,5 +41,6 @@ ActiveRecord::Schema.define(version: 2021_07_19_214633) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "appointments", "lectures"
   add_foreign_key "appointments", "users"
 end
