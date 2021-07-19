de96168be183fa91cb83caf088922ba638231392
Miguel Ricaño <mricanho@gmail.com>
2021-07-19 16:54:29 -0500

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :appointments, dependent: :destroy
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
