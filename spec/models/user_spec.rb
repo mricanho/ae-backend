fc398153527d6659b3b53c6de50095c38174b699
Miguel Ricaño <mricanho@gmail.com>
2021-07-20 15:42:35 -0500

    Add test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'test associations' do
+    it { should have_many(:appointments) }
+  end
+
+  describe 'test validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_length_of(:username).is_at_least(3) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
 end
+
