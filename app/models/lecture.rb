2a6621bfcefb0bf27ec9173a52360e2110e2bd02
Miguel Ricaño <mricanho@gmail.com>
2021-07-28 20:38:49 -0500

    Modify lecture model and controller to accept group and type lectures

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,3 +1,7 @@
 class Lecture < ApplicationRecord
   has_many :appointments, dependent: :destroy
+  has_many :lecture_types, dependent: :destroy
+  has_many :group, through: :lecture_types, source: :lecture_group
+
+  scope :all_with_associations, -> { all.includes(:group) }
 end
